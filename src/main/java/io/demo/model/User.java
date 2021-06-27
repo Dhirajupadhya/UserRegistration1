fafad00ebf4eb40c6d6ed7a70f20cc15a49e805d
Dhiraj Upadhya <dhirajupadhya1997@gmail.com>
2021-06-27 07:56:29 +0530

    Login And Registartion With Complete Sign Up and login lgout Feature

---
+++ src/main/java/io/demo/model/User.java
@@ -0,0 +1,64 @@
+package io.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer user_id;
+	
+	private String user_name;
+	private String user_email;
+	private String user_pass;
+	private String user_mobile;
+	
+	public Integer getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_pass() {
+		return user_pass;
+	}
+	public void setUser_pass(String user_pass) {
+		this.user_pass = user_pass;
+	}
+	public String getUser_mobile() {
+		return user_mobile;
+	}
+	public void setUser_mobile(String user_mobile) {
+		this.user_mobile = user_mobile;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"user_id=" + user_id +
+				", user_name='" + user_name + '\'' +
+				", user_email='" + user_email + '\'' +
+				", user_pass='" + user_pass + '\'' +
+				", user_mobile='" + user_mobile + '\'' +
+				'}';
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into User (user_id,user_name,user_email,user_pass,user_mobile) values (1,'Aadeesh','aadeeshjain.91a@yahoo.com','abcd@234','9876543212');
+insert into User (user_id,user_name,user_email,user_pass,user_mobile) values (2,'Andrew','andrewjate.df@yahoo.com','andrew@9','9876547542');
+insert into User (user_id,user_name,user_email,user_pass,user_mobile) values (3,'John','john619.91a@yahoo.com','john@3113','9876544444');
+insert into User (user_id,user_name,user_email,user_pass,user_mobile) values (4,'Peter','peter.parker@gmail.com','pet_er@9','9871117542');
